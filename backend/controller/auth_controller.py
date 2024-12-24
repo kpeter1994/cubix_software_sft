7ebbb2963e64cd895c14397b2005e5837ee9756e
Kovács Péter <104296687+kpeter1994@users.noreply.github.com>
2024-12-24 15:46:49 +0100

    add toml file

--- backend/controller/auth_controller.py
+++ backend/controller/auth_controller.py
@@ -5,9 +5,12 @@ from datetime import datetime, timedelta, timezone
 SECRET_KEY = "your_secret_key"
 
 class AuthController:
+
     def register(self, username, password):
+
         if User.get_by_username(username):
             return {"error": "Ez a felhasználónév már foglalt"}
+
         user = User.create(username, password)
         return {"id": user.id, "username": user.username}
 
@@ -19,18 +22,24 @@ class AuthController:
         return jwt.encode(payload, SECRET_KEY, algorithm="HS256")
 
     def login(self, username, password):
+
         user = User.get_by_username(username)
+
         if user and user.check_password(password):
             token = self.generate_token(user.id)
             return {"token": token, "message": "Login successful"}
+
         return {"error": "Invalid username or password"}
 
     def verify_token(self, token):
+
         try:
             payload = jwt.decode(token, SECRET_KEY, algorithms=["HS256"])
             return payload
+
         except jwt.ExpiredSignatureError:
             return {"error": "Token has expired"}
+
         except jwt.InvalidTokenError:
             return {"error": "Invalid token"}
 
