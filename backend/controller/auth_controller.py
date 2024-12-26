ce69dfccd6b8c4e7005bb33feb1e239532db56a4
Kovács Péter <smitpeter777@gmail.com>
2024-12-26 10:38:45 +0100

    change static method

--- backend/controller/auth_controller.py
+++ backend/controller/auth_controller.py
@@ -6,8 +6,8 @@ from datetime import datetime, timedelta, timezone
 SECRET_KEY = "7k22c;34WN_"
 
 class AuthController:
-
-    def register(self, username, password, name):
+    @staticmethod
+    def register( username, password, name):
 
         if User.get_by_username(username):
             return jsonify({"error": "A felhasználónév már foglalt"}), 409
@@ -15,7 +15,7 @@ class AuthController:
         user = User.create_user(username, password, name)
 
         if user and user.check_password(password):
-            token = self.generate_token(user.id)
+            token = AuthController.generate_token(user.id)
             return {
                 "token": token,
                 "id": user.id,
@@ -24,26 +24,27 @@ class AuthController:
                 "message": "Register successful"
             }
 
-    def login(self, username, password):
+    @staticmethod
+    def login( username, password):
 
         user = User.get_by_username(username)
 
         if user and user.check_password(password):
-            token = self.generate_token(user.id)
+            token = AuthController.generate_token(user.id)
             return {"token": token, "message": "Login successful"}
 
         return {"error": "Invalid username or password"}
 
-    def generate_token(self, user_id):
+    @staticmethod
+    def generate_token( user_id):
         payload = {
             "user_id": user_id,
             "exp": datetime.now(timezone.utc) + timedelta(hours=1)
         }
         return jwt.encode(payload, SECRET_KEY, algorithm="HS256")
 
-
-
-    def verify_token(self, token):
+    @staticmethod
+    def verify_token(token):
 
         try:
             payload = jwt.decode(token, SECRET_KEY, algorithms=["HS256"])
